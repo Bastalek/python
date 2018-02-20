6b0247e1a416088e4c9a33c665ddf25da1d5b129
Daniel <gaba.dani@gmail.com>
2018-02-20 19:52:34 +0100

    last tuesday

---
+++ lib/modulo1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Pedir el nombre apellidos y mostrar iniciales
+
+def saludar(saludo):
+	print saludo
+
+def iniciales(nombre,ape1,ape2)
+	import sys
+	nombre=sys.argv[1]
+	ape1=sys.argv[2]
+	ape2=sys.argv[3]
+	iniciales= sys.argv[1][0]+','+sys.argv[2][0]+','+sys.argv[3][0]+'.'
+	print "Tus iniciales son " +iniciales.upper()
+
+#def ini (nombre,ap1,ap2)
+#	ini=nombre[0]+ap1[0]+ap2[0]
+#	print "Tus iniciales son " +ini.upper()
+
